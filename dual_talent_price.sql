2869a86f297444bc1b4fa5e0d704ad3335c3f9e0
der_metzger <55544361+der-metzger@users.noreply.github.com>
2020-05-31 01:30:41 +0200

    Create dual_talent_price.sql

---
+++ dual_talent_price.sql
@@ -0,0 +1,6 @@
+/*
+ Affordable Dual Talent Specialization
+ von der_metzger https://github.com/der-metzger
+*/
+
+UPDATE gossip_menu_option SET BoxMoney = 10000 WHERE OptionBroadcastTextID = 33765;
